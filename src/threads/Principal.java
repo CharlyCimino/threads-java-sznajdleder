82fc896ff2ad180c172e773425e44a9d4bf4090f
Charly Cimino <carlosecimino@gmail.com>
2021-06-06 18:22:46 -0300

    Establecer prioridad

--- src/threads/Principal.java
+++ src/threads/Principal.java
@@ -11,6 +11,9 @@ public class Principal {
     public static void main(String[] args) throws InterruptedException {
         DemoThread t1 = new DemoThread("Pablo");
         DemoThread t2 = new DemoThread("Pedro");
+        t1.setPriority(Thread.MAX_PRIORITY);
+        System.out.println(Thread.MAX_PRIORITY);
+        System.out.println(Thread.MIN_PRIORITY);
         t1.start();
         t2.start();
     }
